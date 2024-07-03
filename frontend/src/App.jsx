ea919267fc25552687fd0634160c63d043af8087
Umair-asif84 <umairasif384@gmail.com>
2024-07-03 17:16:23 +0400

    Payment Integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
+import Verify from './pages/Verify/Verify'
+
+
 
 const App = () => {
 
@@ -19,11 +22,14 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
-        <Route path='/order' element={<PlaceOrder />} />
+        <Route path='/order' element={<PlaceOrder/>} />
+        <Route path='/verify' element={<Verify/>} />
+        
 
       </Routes>
 
-    </div><Footer />
+    </div>
+    <Footer />
     </>
   )
 }
